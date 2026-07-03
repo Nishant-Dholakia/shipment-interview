d69cdc01fe397511a1a9a6ac99c65953cba40b28
Nishant Dholakia <nishantdholakia2020@gmail.com>
2026-07-03 09:42:26 +0530

    Initial Boilerplate Commit

---
+++ src/main/java/com/example/java/exception/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.example.java.exception;
+
+import com.example.java.dto.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(
+            ResourceNotFoundException ex) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(
+                        LocalDateTime.now(),
+                        404,
+                        ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(
+            BadRequestException ex) {
+
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(
+                        LocalDateTime.now(),
+                        400,
+                        ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> validation(
+            MethodArgumentNotValidException ex){
+
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(),
+                                error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAll(Exception ex){
+
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse(
+                        LocalDateTime.now(),
+                        500,
+                        ex.getMessage()
+                ));
+    }
+}
